a765ccdfd246ae8535bc2c6310c3556c0320851e
nunoarmando <nunoarmando@gmail.com>
2017-04-12 02:37:12 +0100

    Fixes

--- src/Log.php
+++ src/Log.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace naas\log;
+namespace Naas\Helpers;
 
 /*!
  * Log library
@@ -17,9 +17,10 @@ class Log
 	private $logFile = '';
 	private $logErrorFile = '';
 	
-	public function __construct($options)
+	public function __construct($options = [])
 	{
-		$this->logDir = (isset($options['logDir']))? $options['logDir']: dirname(__DIR__) . '/logs/';
+		global $argv;
+		$this->logDir = (isset($options['logDir']))? $options['logDir']: dirname(dirname(realpath($argv[1]))) . '/logs/';
 		$this->logFile = (isset($options['logFile']))? $options['logFile']: 'log.txt';
 		$this->logErrorFile = (isset($options['logErrorFile']))? $options['logErrorFile']: 'logError.txt';
 		
@@ -28,23 +29,23 @@ class Log
 		}
 	}
 	
-	public function logIt($type, $message, $ok = true, $logfile = 'log') {
+	public function log($type, $message, $ok = true, $logfile = 'log') {
 		$st_message =
 				date('Ymd_His') .
 				'_' . $type . '_' . (($ok) ? 'OK' : 'NOK') . ': ' . $message .
 				"\r\n";
 		
 		if ($logfile == 'err') {
-			file_put_contents(self::$logDir . self::$logErrorFile, $st_message, FILE_APPEND);
+			file_put_contents($this->logDir . $this->logErrorFile, $st_message, FILE_APPEND);
 		} else { //default - log file
-			file_put_contents(self::$logDir . self::$logFile, $st_message, FILE_APPEND);
+			file_put_contents($this->logDir . $this->logFile, $st_message, FILE_APPEND);
 		}
 	}
 	
 	public static function logIt($type, $message, $ok = true, $logfile = 'log')
 	{
-		$log = LogFactory::create();
-		$log->logIt($type, $message, $ok, $logfile);
+		$log = new Log();
+		$log->log($type, $message, $ok, $logfile);
 	} 
 	
 }
@@ -53,8 +54,9 @@ class LogFactory
 {
     public static function create($options = [])
     {
+		global $argv;
         return new Log([
-			'logDir' => (isset($options['logDir']))? $options['logDir']: dirname(__DIR__) . '/logs/',
+			'logDir' => (isset($options['logDir']))? $options['logDir']: dirname(dirname(realpath($argv[1]))) . '/logs/',
 			'logFile' => (isset($options['logFile']))? $options['logFile']: 'log.txt',
 			'logErrorFile' => (isset($options['logErrorFile']))? $options['logErrorFile']: 'logError.txt',
 		]);
